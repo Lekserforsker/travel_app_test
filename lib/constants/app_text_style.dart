f3c39439eae9ba0f72e2fefe91123de3087e19d0
ahmadelraqab <amjrr99@gmail.com>
2024-09-10 01:24:41 +0300

    FIX/ pushing app_text_style

---
+++ lib/constants/app_text_style.dart
@@ -0,0 +1,364 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTextStyle {
+  static const String fontFamily = 'Poppins';
+
+  static TextStyle get bold10 {
+    return const TextStyle(
+      fontSize: 10,
+      color: Colors.black,
+      fontWeight: FontWeight.w700,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get bold12 {
+    return const TextStyle(
+      fontSize: 12,
+      color: Colors.black,
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle get bold14 {
+    return const TextStyle(
+      fontSize: 14,
+      color: Colors.black,
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle get bold32 {
+    return const TextStyle(
+      fontSize: 32,
+      color: Colors.black,
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle get bold36 {
+    return const TextStyle(
+      fontSize: 36,
+      color: Colors.black,
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle get bold8 {
+    return const TextStyle(
+      fontSize: 8,
+      color: Colors.black,
+      fontWeight: FontWeight.w700,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get bold20 {
+    return const TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w700,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get bold24 {
+    return const TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.w700,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get bold28 {
+    return const TextStyle(
+      fontSize: 28,
+      fontWeight: FontWeight.w700,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get bold16 {
+    return const TextStyle(
+      fontWeight: FontWeight.w700,
+      fontSize: 16,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get bold18 {
+    return const TextStyle(
+      fontWeight: FontWeight.w700,
+      fontSize: 18,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get semiBold14 {
+    return const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get semiBold24 {
+    return const TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get semiBold28 {
+    return const TextStyle(
+      fontSize: 28,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get semiBold16 {
+    return const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get semiBold20 {
+    return const TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get semiBold18 {
+    return const TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get semiBold8 {
+    return const TextStyle(
+      fontSize: 8,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get semiBold12 {
+    return const TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get regular10 {
+    return const TextStyle(
+      fontSize: 10,
+      color: Colors.black,
+      fontWeight: FontWeight.w400,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get regular12 {
+    return const TextStyle(
+      fontSize: 12,
+      color: Colors.black,
+      fontWeight: FontWeight.w400,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get regular14 {
+    return const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get regular16 {
+    return const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get regular18 {
+    return const TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.w400,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get regular20 {
+    return const TextStyle(
+      fontSize: 20,
+      color: Colors.black,
+      fontWeight: FontWeight.w400,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get regular22 {
+    return const TextStyle(
+      fontSize: 20,
+      color: Colors.black,
+      fontWeight: FontWeight.w400,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get regular24 {
+    return const TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.w400,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get medium16 {
+    return const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get medium12 {
+    return const TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w500,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get medium10 {
+    return const TextStyle(
+      fontSize: 10,
+      fontWeight: FontWeight.w500,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get medium14 {
+    return const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get medium18 {
+    return const TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.w500,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get medium20 {
+    return const TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w500,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get medium22 {
+    return const TextStyle(
+      fontSize: 22,
+      fontWeight: FontWeight.w500,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get medium24 {
+    return const TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.w500,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get medium28 {
+    return const TextStyle(
+      fontSize: 28,
+      fontWeight: FontWeight.w500,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get medium32 {
+    return const TextStyle(
+      fontSize: 32,
+      fontWeight: FontWeight.w500,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get extraBold24 {
+    return const TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.w800,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get extraBold36 {
+    return const TextStyle(
+      fontSize: 36,
+      fontWeight: FontWeight.w800,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+
+  static TextStyle get it12 {
+    return const TextStyle(
+      fontSize: 12,
+      color: Colors.black,
+      fontFamily: fontFamily,
+    );
+  }
+}
