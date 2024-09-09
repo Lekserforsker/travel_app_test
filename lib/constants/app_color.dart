8ba07167f61661124d94fdd8defb5bb6cd146b09
ahmadelraqab <amjrr99@gmail.com>
2024-09-10 00:27:39 +0300

    adding app color class to use it across the app

---
+++ lib/constants/app_color.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const taPinkE8536D = Color(0XFFE8536D);
+  static const taBlack1F1F1F = Color(0XFF1F1F1F);
+  static const taBlack191919 = Color(0XFF191919);
+  static const taBlack212B36 = Color(0XFF212B36);
+  static const taBlack2E3132 = Color(0XFF2E3132);
+  static const taWhiteFFFFFF = Color(0XFFFFFFFF);
+  static const taWhiteFFF3F3F3 = Color(0XFFF3F3F3);
+  static const taGrey7C7C7C = Color(0XFF7C7C7C);
+  static const taGrey747474 = Color(0XFF747474);
+  static const taGrey454545 = Color(0XFF454545);
+  static const taRedFF3333 = Color(0XFFFF3333);
+  static const taBlue323C43 = Color(0XFF323C43);
+}
