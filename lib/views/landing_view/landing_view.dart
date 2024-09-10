0e1dce5c1a6894f7895d52c21524a0291311c5dd
ahmadelraqab <amjrr99@gmail.com>
2024-09-10 09:13:37 +0300

    NEW/ add the upload post popup

--- lib/views/landing_view/landing_view.dart
+++ lib/views/landing_view/landing_view.dart
@@ -4,6 +4,7 @@ import 'package:travel_app_test/constants/app_color.dart';
 import 'package:travel_app_test/constants/app_text_style.dart';
 import 'package:travel_app_test/views/home_view/home_view.dart';
 import 'package:travel_app_test/views/landing_view/widgets/app_bar_widget.dart';
+import 'package:travel_app_test/widgets/text_button_widget.dart';
 import '../../constants/app_svg.dart';
 
 class LandingView extends StatefulWidget {
@@ -23,6 +24,18 @@ class _LandingViewState extends State<LandingView> {
     (title: "Promotion", path: AppSvg.discountTag),
     (title: "Profile", path: AppSvg.userCircle),
   ];
+
+  final pages = [
+    const HomeView(),
+    Container(),
+    Container(),
+    Container(),
+  ];
+
+  bool animate = true;
+
+  void toggleAnimation() => setState(() => animate = !animate);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +44,13 @@ class _LandingViewState extends State<LandingView> {
       floatingActionButton: Container(
         margin: const EdgeInsets.only(bottom: 20),
         decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          border: !animate
+              ? null
+              : Border.all(
+                  width: 2,
+                  color: AppColor.taWhiteFFFFFF,
+                ),
           boxShadow: [
             BoxShadow(
                 blurRadius: 15,
@@ -38,11 +58,14 @@ class _LandingViewState extends State<LandingView> {
                 color: AppColor.taBlack000000.withOpacity(.15)),
           ],
         ),
-        child: CircleAvatar(
-          radius: 26,
-          backgroundColor: AppColor.taPinkE8536D,
-          child: Center(
-            child: SvgPicture.asset(AppSvg.plus),
+        child: InkWell(
+          onTap: () => toggleAnimation(),
+          child: CircleAvatar(
+            radius: 26,
+            backgroundColor: AppColor.taPinkE8536D,
+            child: Center(
+              child: SvgPicture.asset(AppSvg.plus),
+            ),
           ),
         ),
       ),
@@ -50,7 +73,80 @@ class _LandingViewState extends State<LandingView> {
       body: SafeArea(
         child: Stack(
           children: [
-            const HomeView(),
+            pages[activeIndex],
+            AnimatedPositioned(
+              duration: const Duration(milliseconds: 150),
+              bottom: animate ? 100 : 0,
+              left: 50,
+              right: 50,
+              child: AnimatedScale(
+                duration: const Duration(milliseconds: 150),
+                scale: animate ? 1 : 0,
+                child: Container(
+                  width: 315,
+                  height: 160,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 10,
+                          color: AppColor.taGrey7C7C7C.withOpacity(.25),
+                          offset: const Offset(0, 5)),
+                    ],
+                  ),
+                  child: ClipPath(
+                    clipper: CenterClip(),
+                    child: Container(
+                      width: 315,
+                      height: 160,
+                      color: AppColor.taWhiteFFFFFF,
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Upload New Post",
+                            style: AppTextStyle.bold16.copyWith(
+                              color: AppColor.taBlack1F1F1F,
+                            ),
+                          ),
+                          const SizedBox(height: 6),
+                          Text(
+                            "Click “+” Icon to create new post",
+                            style: AppTextStyle.medium14.copyWith(
+                              color: AppColor.taGrey7C7C7C,
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              TextButtonWidget(
+                                onTap: () => {},
+                                size: const Size(140, 35),
+                                borderRadius: 100,
+                                style: AppTextStyle.semiBold14
+                                    .copyWith(color: AppColor.taWhiteFFFFFF),
+                                title: 'Regular post',
+                              ),
+                              TextButtonWidget(
+                                onTap: () => {},
+                                size: const Size(140, 35),
+                                borderColor: AppColor.taPinkE8536D,
+                                color: AppColor.taWhiteFFFFFF,
+                                style: AppTextStyle.medium14
+                                    .copyWith(color: AppColor.taPinkE8536D),
+                                borderRadius: 100,
+                                title: 'Partner post',
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
             Align(
               alignment: Alignment.bottomCenter,
               child: Container(
@@ -59,7 +155,6 @@ class _LandingViewState extends State<LandingView> {
                 width: MediaQuery.sizeOf(context).width,
                 child: Row(
                   children: [
-                    // ...categories.indexed.map
                     for (final (index, obj) in categories.indexed) ...[
                       if (index == 2) const Expanded(child: SizedBox()),
                       Expanded(
@@ -97,5 +192,59 @@ class _LandingViewState extends State<LandingView> {
         ),
       ),
     );
+  }
+}
+
+class CenterClip extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.moveTo(15, 0);
+    path.lineTo(size.width - 15, 0);
+
+    path.quadraticBezierTo(
+      size.width,
+      0,
+      size.width,
+      15,
+    );
+
+    path.lineTo(size.width, size.height - 15 - 12);
+
+    path.quadraticBezierTo(
+      size.width,
+      size.height - 12,
+      size.width - 15,
+      size.height - 12,
+    );
+
+    path.lineTo(size.width / 2 + 12, size.height - 12);
+    path.lineTo(size.width / 2, size.height);
+    path.lineTo(size.width / 2 - 12, size.height - 12);
+
+    path.lineTo(15, size.height - 12);
+
+    path.quadraticBezierTo(
+      0,
+      size.height - 12,
+      0,
+      size.height - 12 - 15,
+    );
+    path.lineTo(0, 15);
+
+    path.quadraticBezierTo(
+      0,
+      0,
+      15,
+      0,
+    );
+
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
